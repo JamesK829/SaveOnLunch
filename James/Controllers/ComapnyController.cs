f2b28e101917f29edcd893f7f4e75a10fb1214c3
jameshao666 <kmter222@hotmail.com>
2021-03-16 14:47:27 +0800

    test

---
+++ James/Controllers/ComapnyController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using James.Models;
+using James.ViewModels;
+
+namespace James.Controllers
+{
+    public class ComapnyController : Controller
+    {
+        // GET: Comapny
+
+        dbjEntities1 db = new dbjEntities1();
+
+        public ActionResult CompanyTransactions()
+        {
+
+
+            var tList = db.transaction.OrderBy(x => x.tId);
+            //var trans = new List<trans>();
+            var T = (from t in db.transaction
+                     join c in db.Customer on t.cId equals c.cId
+                     join o in db.OrderList on t.cId equals o.cId
+
+
+                     select new trans
+                     {
+                         cId = (int)t.cId,
+                         customerName = c.cFName + c.cLName,
+                         customerEmail = c.cEmail,
+                         companyIncome = (decimal)t.companyIncome,
+                         transactionDate = (DateTime)t.tDate
+
+
+
+
+                     }).ToList();
+            ViewBag.TotalIncome = T.Sum(x => x.companyIncome).ToString("0.##");
+
+            var vm = new vTransactionVM
+            {
+                trans = (List<trans>)T,
+                total = T.Sum(x => x.companyIncome)
+
+
+            };
+
+            return View();
+
+        }
+    }
+}
---
+++ James/ViewModels/Products/vProductListVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using James.Models;
+using PagedList;
+using System.Web.Mvc;
+
+namespace James.ViewModels
+{
+    public class vProductListVM
+    {
+        public ProductSearchModel searchParameter { get; set; }
+
+        public int pageIndex { get; set; }
+
+        public IPagedList<Product> products { get; set; }
+
+
+        public SelectList styleItems { get; set; }
+
+        public int ordernum { get; set; }
+
+    }
+    public class ProductSearchModel
+    {
+        public string style { get; set; }
+        public string productName { get; set; }
+    }
+}
